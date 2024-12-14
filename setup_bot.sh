610870b1de49f9a443364d7603c891909c9ead73
Kurama <60360593+Kurama250@users.noreply.github.com>
2024-12-14 21:25:57 +0100

    Update setup_bot.sh

--- setup_bot.sh
+++ setup_bot.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-# setup_bot.sh by Kurama250
-# Github : https://github.com/Kurama250
+# setup.sh by Kurama250
+# Github : https://github.com/Kurama250/
 
 apt update && apt upgrade -y
 apt install npm nodejs git -y
+
 if ! command -v node &> /dev/null; then
-    curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
-    apt-get install -y nodejs -y
+    echo "Node.js is not installed. Installing..."
+    curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+    apt-get install -y nodejs
 else
     echo "Node.js is already installed. Skipping installation."
 fi
+
 git clone https://github.com/Kurama250/Ghost_bot.git
 cd Ghost_bot/
 npm install discord.js@13 fs
-npm install pm2 -g
+
+if ! command -v pm2 &> /dev/null; then
+    echo "PM2 is not installed. Installing..."
+    npm install pm2 -g
+else
+    echo "PM2 is already installed. Skipping installation."
+fi
